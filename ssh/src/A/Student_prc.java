0653064597949b881e64dc2169e44424244541c6
dtezxtbb <1042474344@qq.com>
2020-06-10 11:12:15 +0800

    提示提交到本地仓库

---
+++ ssh/src/A/Student_prc.java
@@ -0,0 +1,20 @@
+package A;
+
+public class Student_prc {
+
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	
+	public Student_prc(int id,String name,String password) {
+		
+		this.id=id;
+		this.username=username;
+	}
+	
+	
+	
+}
